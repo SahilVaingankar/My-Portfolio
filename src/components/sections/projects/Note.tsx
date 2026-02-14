72b0a9155fa6327dedfd4be57ac715d5d6a980a1
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2026-02-14 12:05:34 +0530

    updated content in  Note component

--- src/components/sections/projects/Note.tsx
+++ src/components/sections/projects/Note.tsx
@@ -13,7 +13,39 @@ const Note = () => {
         Note: The backend for this project was originally deployed using
         Railway’s free tier. Since the trial expired, the server is no longer
         active. However, the frontend remains fully functional and the complete
-        source code (both frontend and backend) is available in the repository.{" "}
+        source code (both frontend and backend) is available in the repository.
+        <br />
+        If you would prefer to review live implementations, the projects linked
+        below are fully deployed. They are comparatively smaller in complexity
+        but demonstrate complete working systems. <br />
+        <br />
+        Dashboard: basic dasboard UI with full CI/CD pipeline implementation.
+        frontend only project <br />
+        Link:{" "}
+        <a
+          href="https://dashboard-hazel-three-71.vercel.app/"
+          target="_blank"
+          className="text-blue-500 underline">
+          https://dashboard-hazel-three-71.vercel.app/
+        </a>
+        <br />
+        <br />
+        Inventory-managemen-system: a simple inventory management system with
+        authentication, role base access and CRUD operations. <br />
+        <br />
+        Demo Accounts:
+        <br /> • admin: admin@gmail.com / pass: admin
+        <br /> • Staff: staffuser@gmail.com / pass: staffuser
+        <br />• User: testuser@gmail.com / pass: 12345 <br />
+        <br />
+        Link:{" "}
+        <a
+          href="https://inventory-management-system-three-xi.vercel.app/"
+          target="_blank"
+          className="text-blue-500 underline">
+          https://inventory-management-system.vercel-three-xi.app/
+        </a>
+        <br />
         <span
           className={`${
             toggle
